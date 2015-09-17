c7073c6caaf9122c3a29c6615aab74b015cf9d25
Joe Siebert <jsiebert@uvm.edu>
2015-09-17 13:52:58 -0400

    Remaining queries are now functional. Code needs comments, CSS needs updating, need to make .sql files, etc.

--- assignment2.0/select.php
+++ assignment2.0/select.php
@@ -22,13 +22,15 @@ if ($queryNumber != "") {
             $query .= " FROM tblTeachers";
             $data = array("");
             $val = array(0, 0, 0, 0);
+            $queryText ="";
             break;
         case 2:
             $query = "SELECT fldDepartment";
             $query .= " FROM tblCourses";
             $query .= " WHERE fldCourseName LIKE ?";
-            $data = array('Introduction%');
+            $data = array('%Introduction%');
             $val = array(1, 0, 0, 0);
+            $queryText = "SELECT fldDepartment FROM tblCourses WHERE fldCourseName LIKE '%Introduction%'";
             break;
         case 3:
             $query = "SELECT fnkCourseId, fldCRN, fnkTeacherNetId, fldMaxStudents, fldNumStudents, fldSection, fldType, fldStart, fldStop, fldDays, fldBuilding, fldRoom";
@@ -36,6 +38,7 @@ if ($queryNumber != "") {
             $query .= " WHERE fldStart = ?";
             $data = array('13:10:00');
             $val = array(1, 0, 0, 0);
+            $queryText = "SELECT fnkCourseId, fldCRN, fnkTeacherNetId, fldMaxStudents, fldNumStudents, fldSection, fldType, fldStart, fldStop, fldDays, fldBuilding, fldRoom FROM tblSections WHERE fldStart = '13:10:00'";
             break;
         case 4:
             $query = "SELECT pmkCourseId, fldCourseNumber, fldCourseName, fldDepartment, fldCredits";
@@ -43,6 +46,7 @@ if ($queryNumber != "") {
             $query .= " WHERE fldDepartment = ? AND fldCourseNumber = ?";
             $data = array('CS', 148);
             $val = array(1, 1, 0, 0);
+            $queryText = "SELECT pmkCourseId, fldCourseNumber, fldCourseName, fldDepartment, fldCredits FROM tblCourses WHERE fldDepartment = 'CS' AND fldCourseNumber = 148";
             break;
         case 5:
             $query = "SELECT fldFirstName, fldLastName";
@@ -50,6 +54,7 @@ if ($queryNumber != "") {
             $query .= " WHERE pmkNetId LIKE ?";
             $data = array('r%o');
             $val = array(1, 0, 0, 0);
+            $queryText = "SELECT fldFirstName, fldLastName FROM tblTeachers WHERE pmkNetId LIKE 'r%o'";
             break;
         case 6:
             $query = "SELECT fldCourseName";
@@ -57,23 +62,59 @@ if ($queryNumber != "") {
             $query .= " WHERE fldCourseName LIKE ? AND fldDepartment <> ?";
             $data = array('%data%', 'CS');
             $val = array(1, 1, 0, 2);
+            $queryText = "SELECT fldCourseName FROM tblCourses WHERE fldCourseName LIKE '%data%' AND fldDepartment <> 'CS'";
             break;
         case 7:
             $query = "SELECT COUNT(DISTINCT fldDepartment)";
             $query .= " FROM tblCourses";
             $data = array("");
             $val = array(0, 0, 0, 0);
+            $queryText ="";
             break;
         case 8:
-            $query = "SELECT DISTINCT fldBuilding, COUNT(fldSection)";
+            $query = "SELECT fldBuilding, COUNT(fldSection)";
             $query .= " FROM tblSections";
             $query .= " GROUP BY fldBuilding";
             $data = array("");
             $val = array(0, 0, 0, 0);
+            $queryText ="";
             break;
         case 9:
+            $query = "SELECT fldBuilding, SUM(fldNumStudents)";
+            $query .= " FROM tblSections";
+            $query .= " WHERE fldDays LIKE ?";
+            $query .= " GROUP BY fldBuilding";
+            $query .= " ORDER BY SUM(fldNumStudents) DESC";
+            $data = array('%W%');
+            $val = array(1, 1, 0, 0);
+            $queryText = "SELECT fldBuilding, SUM(fldNumStudents) FROM tblSections WHERE fldDays LIKE '%W%' GROUP BY fldBuilding ORDER BY SUM(fldNumStudents) DESC";
             break;
         case 10:
+            $query = "SELECT fldBuilding, SUM(fldNumStudents)";
+            $query .= " FROM tblSections";
+            $query .= " WHERE fldDays LIKE ?";
+            $query .= " GROUP BY fldBuilding";
+            $query .= " ORDER BY SUM(fldNumStudents) DESC";
+            $data = array('%F%');
+            $val = array(1, 1, 0, 0);
+            $queryText = "SELECT fldBuilding, SUM(fldNumStudents) FROM tblSections WHERE fldDays LIKE '%F%' GROUP BY fldBuilding ORDER BY SUM(fldNumStudents) DESC";
+            break;
+        case 11:
+            $query = "SELECT fnkCourseId";
+            $query .= " FROM tblSections";
+            $query .= " GROUP BY fnkCourseId";
+            $query .= " HAVING COUNT(fldSection) >= ?";
+            $data = array(50);
+            $val = array(0, 0, 0, 1);
+            $queryText = "SELECT fnkCourseId FROM tblSections GROUP BY fnkCourseId HAVING COUNT(fldSection) >= 50";
+            break;
+        case 12:
+            $query = "SELECT SUM(fldNumStudents - fldMaxStudents) ";
+            $query .= " FROM tblSections";
+            $query .= " WHERE fldNumStudents > fldMaxStudents";
+            $data = array("");
+            $val = array(1, 0, 0, 1);
+            $queryText ="";
             break;
     }
     if ($debug) {
@@ -83,7 +124,13 @@ if ($queryNumber != "") {
 
     if ($info != "") {
         print '<h2>Total Records: ' . count($info) . '</h2>';
-        print '<h3>SQL: ' . $query . '</h3>';
+        print '<h3>SQL: ';
+        if ($queryText != "") {
+            print $queryText;
+        } else {
+            print $query;
+        }
+        print '</h3>';
 
         if ($debug) {
             print "<p>DATA: <pre>";
